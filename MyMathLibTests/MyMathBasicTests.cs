637ca228083f1f19711c335e36e536aca1c8fe6e
Denis Kokorev <nvsbleat@gmail.com>
2018-01-30 19:04:08 +0300

    Добавлен проект для тестов

---
+++ MyMathLibTests/MyMathBasicTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyMathLibTests
+{
+    [TestClass]
+    public class MyMathBasicTests
+    {
+        [TestMethod]
+        public void CreateGrid_0and1and10_1returned()
+        {
+            //задать(настроить) параметры
+
+            //запустить код
+
+            //проверить правильность
+        }
+    }
+}
