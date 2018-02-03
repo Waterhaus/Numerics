258cf8570d462db9da0caa4f0f76c829d717ef02
Denis Kokorev <nvsbleat@gmail.com>
2018-02-03 14:17:28 +0300

    Класс Grid

---
+++ MyMathLib/Grid.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMathLib
+{
+    public enum GridType { Uniform, BSplineStandart}
+
+    public class Grid
+    {
+        private List<double> dots;
+        private int degree;
+        private double a_border;
+        private double b_border;
+
+        public int SplineDegree
+        {
+            get { return degree; }
+        }
+
+        public Grid()
+        {
+            dots = null;
+            degree = 0;
+            a_border = b_border = 0;
+        }
+    }
+}
